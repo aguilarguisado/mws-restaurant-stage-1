d5ac6b85fdd281306ec7c5cf1601eb2cc6167c37
Juan Aguilar Guisado <juaggu@gmail.com>
2018-03-23 19:11:06 +0100

    fix: setting restaurantContent as parent of restaurant elements -but for the image

--- js/main.js
+++ js/main.js
@@ -143,22 +143,27 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  // Developing a container for improving logical and structure of every restaurant
+  const restaurantContent = document.createElement('div');
+  restaurantContent.className = 'restaurant-content';
+  li.append(restaurantContent);
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  restaurantContent.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  restaurantContent.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  restaurantContent.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  restaurantContent.append(more)
 
   return li
 }
